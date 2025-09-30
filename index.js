4cbdc32124e6cccae2a7812df1d5e790a364dbd8
ShadowDevKat <211905275+ShadowDevKat@users.noreply.github.com>
2025-09-30 13:15:36 +0500

    Added tests for
    analyze array and caeser cipher

--- index.js
+++ index.js
@@ -20,3 +20,34 @@ export const calculator = {
         return a * b;
     }
 }
+
+export function analyzeArray(arr) {
+    return {
+        average: arr.reduce((a, b) => a + b) / arr.length,
+        length: arr.length,
+        min: Math.min(...arr),
+        max: Math.max(...arr),
+    }
+}
+
+export function caesarCipher(str, shift) {
+    let result = "";
+    shift = shift % 26;
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+
+        if (char >= 'A' && char <= 'Z') {
+            let charCode = char.charCodeAt(0);
+            let shiftedCharCode = ((charCode - 'A'.charCodeAt(0) + shift + 26) % 26) + 'A'.charCodeAt(0);
+            result += String.fromCharCode(shiftedCharCode);
+        } else if (char >= 'a' && char <= 'z') {
+            let charCode = char.charCodeAt(0);
+            let shiftedCharCode = ((charCode - 'a'.charCodeAt(0) + shift + 26) % 26) + 'a'.charCodeAt(0);
+            result += String.fromCharCode(shiftedCharCode);
+        } else {
+            result += char; 
+        }
+    }
+    return result;
+}
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import { calculator, capitalize, reverseString } from ".";
+import { analyzeArray, caesarCipher, calculator, capitalize, reverseString } from ".";
 
 test("Capitalize first letter of word", () => {
     expect(capitalize('word')).toBe('Word');
@@ -22,4 +22,21 @@ test("Divide: 9/3=3", () => {
 
 test("Mul: 2*3=6", () => {
     expect(calculator.mul(2, 3)).toBe(6);
+});
+
+test('Caesar Cipher xyz become abc', () => {
+    expect(caesarCipher('xyz', 3)).toBe('abc')
+})
+
+test("Caesar Cipher 'Hello, World!' becomes 'Khoor, Zruog!'", () => {
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!')
+})
+
+test("analyzes the array [1,8,3,4,2,6] and provides an average, min, max, and length", () => {
+    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
+        average: 4,
+        length: 6,
+        min: 1,
+        max: 8,
+    });
 });
