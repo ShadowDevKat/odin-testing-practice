7c1682a7960384ecc05b3bfdc8c8d7d0a2953f1b
ShadowDevKat <211905275+ShadowDevKat@users.noreply.github.com>
2025-09-30 13:07:16 +0500

    Added tests and functions for
    reverseString and calculator

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-function capitalize(word) {
+export function capitalize(word) {
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-export { capitalize }
+export function reverseString(word) {
+    return word.split('').reverse().join('')
+}
+
+export const calculator = {
+    add(a, b) {
+        return a + b;
+    },
+    sub(a, b) {
+        return a - b;
+    },
+    div(a, b) {
+        return a / b;
+    },
+    mul(a, b) {
+        return a * b;
+    }
+}
--- main.test.js
+++ main.test.js
@@ -1,5 +1,25 @@
-import { capitalize } from ".";
+import { calculator, capitalize, reverseString } from ".";
 
 test("Capitalize first letter of word", () => {
     expect(capitalize('word')).toBe('Word');
+});
+
+test("Reverse string", () => {
+    expect(reverseString('word')).toBe('drow');
+});
+
+test("Add: 2+2=4", () => {
+    expect(calculator.add(2, 2)).toBe(4);
+});
+
+test("Sub: 3-1=2", () => {
+    expect(calculator.sub(3, 1)).toBe(2);
+});
+
+test("Divide: 9/3=3", () => {
+    expect(calculator.div(9, 3)).toBe(3);
+});
+
+test("Mul: 2*3=6", () => {
+    expect(calculator.mul(2, 3)).toBe(6);
 });
